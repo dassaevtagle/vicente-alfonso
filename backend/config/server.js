095d7759bbd10e7bb54c6369014c33d38b49af83
Dassaev Tagle <dassaev.tagle@gmail.com>
2022-10-09 16:19:53 -0500

    dev prod config strapi

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,19 @@
-module.exports = ({ env }) => ({
+const dev = process.env.NODE_ENV !== 'production'
+
+const devConfig = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+})
+
+const prodConfig = ({ env }) => ({
   proxy: true,
   url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
   app: { 
     keys: env.array('APP_KEYS')
   },
-});
+})
+
+module.exports = dev ? devConfig : prodConfig
