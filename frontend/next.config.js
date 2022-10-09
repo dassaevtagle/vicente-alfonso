ffdeaa9e9f3adadcba2fc217c20c7a0c7934151d
Dassaev Tagle <dassaev.tagle@gmail.com>
2022-10-09 15:35:54 -0500

    disable temporarily eslint errors on prod builds

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,4 +4,9 @@ module.exports = {
     loader: 'default',
     domains: ['localhost'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
